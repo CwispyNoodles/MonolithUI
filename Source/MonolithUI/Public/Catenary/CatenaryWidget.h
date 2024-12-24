f85eef22ea3ee80e5324b8db737d8ab3242305eb
CwispyNoodles <89599702+CwispyNoodles@users.noreply.github.com>
2024-12-24 11:17:38 -0500

    Created CatenaryWidget

---
+++ Source/MonolithUI/Public/Catenary/CatenaryWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/Widget.h"
+#include "CatenaryWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MONOLITHUI_API UCatenaryWidget : public UWidget
+{
+	GENERATED_BODY()
+	
+};
