fdcba7807a882fda66d6ce6a1888b4244322f760
CwispyNoodles <89599702+CwispyNoodles@users.noreply.github.com>
2024-12-31 13:59:47 -0500

    added descriptions for each property in CatenaryArguments

--- Source/MonolithUI/Public/Catenary/SlateCatenary.h
+++ Source/MonolithUI/Public/Catenary/SlateCatenary.h
@@ -53,24 +53,42 @@ struct MONOLITHUI_API FCatenaryArguments
 		&& Brush == Other.Brush;
 	}
 
+	// Defines the behavior of Point 1. 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, DisplayName="Point 1 Connection Schema")
 	FCatenaryConnectionSchema P1ConnectionSchema;
 
+	// Defines the behavior of Point 2. 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, DisplayName="Point 2 Connection Schema")
 	FCatenaryConnectionSchema P2ConnectionSchema;
-	
+
+	// The Length of the Catenary.
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	float Length = 500.0f;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	int Iterations = 15;
-
+	// Number of Segments rendered on the Catenary.
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta=(ClampMin=1))
 	int Segments = 10;
 
+	// The equation used for solving for a particular Catenary is 'y = a cosh((x-p)/a)+q'
+	// where 'p' and 'q' are x and y offsets and 'a' is the size and scale of the Catenary.
+	// 'a' has no closed form solution so we must iteratively solve for it.
+	// IntervalStep helps us by first calculating a rough initial estimate for 'a', before we fine tune it
+	// on a later step.
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta=(ClampMin=0.01f))
 	float IntervalStep = 5.0f;
 
+	// The equation used for solving for a particular Catenary is 'y = a cosh((x-p)/a)+q'
+	// where 'p' and 'q' are x and y offsets and 'a' is the size and scale of the Catenary.
+	// 'a' has no closed form solution so we must iteratively solve for it.
+	// When we fine tune a rough estimate of 'a' we iteratively refine our estimate via bisection.
+	// Iterations is the maximum number of bisections should we not arrive at our desired Precision for 'a'
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int Iterations = 15;
+
+	// The equation used for solving for a particular Catenary is 'y = a cosh((x-p)/a)+q'
+	// where 'p' and 'q' are x and y offsets and 'a' is the size and scale of the Catenary.
+	// 'a' has no closed form solution so we must iteratively solve for it.
+	// Precision is the minimum desired precision we iteratively refine our rough estimate of 'a'.
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta=(ClampMin=0.0001f))
 	float Precision = 0.0001f;
 
